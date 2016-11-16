e8520a89e26821db4284603ff48ddeef848f91f7
Petter Kraabøl <petter.zarlach@gmail.com>
2016-11-16 14:46:15 +0100

    Added settings file and improved user experience for convenience

--- app.py
+++ app.py
@@ -1,16 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import requests, sys, time, os
+import requests, sys, time, os, json, shutil
 
+# Get video ID
+if len(sys.argv) >= 2:
+    videoId = 'v' + sys.argv[1].replace('v', '')
+else:
+    videoId = 'v' + raw_input('Video ID: ').replace('v', '')
 
-# Video id input (prefixed 'v' in the parameter is is optional).
-videoId = 'v' + sys.argv[1].replace('v', '')
+# Program requires at least settings.example.json or settings.json to run properly
+if not os.path.isfile('example.settings.json') and not os.path.isfile('settings.json'):
+    print 'Error: Missing settings file.'
+    sys.exit(1)
+
+# Copy settings example file if settings.json doesn't exist
+if not os.path.isfile('settings.json'):
+    shutil.copyfile('settings.example.json', 'settings.json')
+
+# Load settings
+with open('settings.json', 'r') as settings_file:
+    settings = json.load(settings_file)
+
+# Check if a client_id was provided as an argument
+if len(sys.argv) >= 3:
+    if not sys.argv[2] == settings['client_id']:
+        settings['client_id'] = sys.argv[2]
+        answer = raw_input('Save client ID? (Y/n): ')
+        if (not answer.lower() == "n"):
+            with open('settings.json', 'w') as settings_file:
+                json.dump(settings, settings_file)
+
+# Check if client_id is required
+if settings['require_client_id'] and not settings['client_id']:
+    print "Twitch requires a client ID to use their API.\nRegister an application on https://www.twitch.tv/settings/connections to get yours."
+    settings['client_id'] = raw_input('Client ID: ')
+    answer = raw_input('Save client ID? (Y/n): ')
+    if (not answer.lower() == "n"):
+        with open('settings.json', 'w') as settings_file:
+            json.dump(settings, settings_file)
+
+# Get client id parameter for URL
+def getClientIdParameter():
+    if (settings['client_id']):
+        return 'client_id='+settings['client_id'] + '&'
+    else:
+        return ''
+
+# API URL
+apiUrl = 'https://rechat.twitch.tv/rechat-messages'
 
 # Get start and stop time by looking at the 'detail' message from Twitch
 #
 # If you query this API with invalid an invalid timestamp (none or out of range),
 # it will tell you the start and stop timestamp, however, in text format.
-response = requests.get('https://rechat.twitch.tv/rechat-messages?start=0&video_id=' + videoId).json()
+response = requests.get(apiUrl + '?' + str(getClientIdParameter()) + 'start=0&video_id=' + videoId).json()
 
 # Parse response for start and stop
 #
@@ -27,6 +70,17 @@ response = requests.get('https://rechat.twitch.tv/rechat-messages?start=0&video_
 # As the start and stop timestamp is (for some weird reason)
 # in text format, we have to parse the response.
 detail = response['errors'][0]['detail'].split(' ') # We split the detail string into an array
+
+# Check if valid video ID
+# If the length is 8, it's (most likely) invalid
+# If the length is 7, it's (most likely) valid
+if len(detail) != 7:
+    if settings['require_client_id']:
+        print 'Error: Invalid video or client ID'
+    else:
+        print 'Error: Invalid video ID'
+    sys.exit(1)
+
 start = int(detail[4])                              # The start timestamp is on index 4
 stop = int(detail[6])                               # while stop has the index 6
 
@@ -43,7 +97,7 @@ messageIds = []
 # This is where we save the messages.
 
 # Output directory
-directory = 'chats'
+directory = settings['folder']
 if not os.path.exists(directory):
     os.makedirs(directory)
 
@@ -56,7 +110,7 @@ timestamp = start
 while timestamp <= stop:
 
     # Request messages from Twitch
-    response = requests.get('https://rechat.twitch.tv/rechat-messages?start=' + str(timestamp) + '&video_id=' + videoId).json()
+    response = requests.get(apiUrl + '?start=' + str(timestamp) + '&video_id=' + videoId).json()
     data = response['data'];
 
     # Increase by one (will be overwritten if new messages are found).
@@ -78,6 +132,25 @@ while timestamp <= stop:
             # Append message to output file
             file.write(date + ' ' + sender + ': ' + text + '\n')
 
+            # Print messages, if not, show progress
+            if settings['print']:
+
+                # Print messages
+                print '\033[94m' + date + ' \033[92m'+ sender + '\033[0m' + ': ' + text
+            else:
+
+                #Show progress %
+                progress = timestamp - start
+                done = stop - start
+                percentage = round(progress*100 / float(done), 2)
+
+                # Bigfix: percentage goes slightly above 100
+                if percentage > 100.0:
+                    percentage = 100.0
+
+                sys.stdout.write('Downloading... (' + str(percentage) + '%)\r')
+                sys.stdout.flush()
+
             # Set timestamp to this message's timestamp to improve
             # performance and skip timestamps where no new messages are coming in.
             #
@@ -85,8 +158,6 @@ while timestamp <= stop:
             # query does not want the last 3 digits (for whatever reason)
             timestamp = int(message['attributes']['timestamp']/1000)
 
-            # Print to console (optional)
-            print '\033[94m' + date + ' \033[92m'+ sender + '\033[0m' + ': ' + text
-
 # Close file
 file.close()
+sys.stdout.write('Finished downloading ' + videoId + '\r')
--- readme.md
+++ readme.md
@@ -3,18 +3,33 @@
 Neat python script to download chat messages from past broadcasts
 
 ### Requirements
+
 - [Python 2.7](https://www.python.org/downloads/)
 - [pip](https://pip.pypa.io/en/stable/installing/)
-- ```pip install requests``` [Requests library installation guide](http://docs.python-requests.org/en/master/user/install/)
+- [Requests library installation guide](http://docs.python-requests.org/en/master/user/install/)
+
+### Install requirements
+```bash
+pip install -r requirements.txt
+```
 
 ### Usage
+
 ```bash
-$ python app.py 80123392
+python app.py 80123392
 ```
 *Now, just replace the number with any video id, found in the url of the vod.*
 
-### Important
+### Settings
+
+When running the program for the first time, `example.settings.json` will be copied to `settings.json`. The command below sets video and client ID, however, they are both optional parameters when running the script.
+```bash
+python app.py <video_id> <client_id>
+```
+
+### Notes
+
+- A client ID is currently not required by Twitch. If they eventually decides to require a client ID, `require_client_id` in `settings.json` should be set to `true` to require a client ID when running the script.
 - Empty messages means the user has been timed out. There's no known way to get these messages.
-- This script is using Twitch's unofficial and undocumented ReChat API. The script may break at any time.
-- [As of August 8th 2016](https://discuss.dev.twitch.tv/t/client-id-requirement-faqs/6108), Twitch will require a client-id to be sent with every API call. This may (not) affect the ReChat API.
+- This script is using Twitch's undocumented ReChat API. The script may break at any time.
 - The longer VOD, the more API calls are made to Twitch (possibly thousands). Use with care and stay up to date on any rate limits.
